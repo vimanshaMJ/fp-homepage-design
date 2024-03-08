094954f83a3f02e79b791e7cbd0ec035e79fc99f
vimansha <vimanshamadushani88@gmail.com>
2024-03-09 01:27:36 +0530

    feat: start navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,32 +1,100 @@
 import * as React from "react";
+import classes from "./Navbar.module.css";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import AdbIcon from "@mui/icons-material/Adb";
+import avatar from "../Images/girl.png";
 
-export default function ButtonAppBar() {
+export default function Appbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <AppBar position="static" className={classes.appbar}>
+      <Container minWidth="xl">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+          <Typography
+            variant="h4"
+            sx={{
+              display: { xs: "none", md: "flex" },
+              fontFamily: "lato",
+              fontWeight: 700,
+              color: "#5A189A",
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Funerals
+            <span className={classes.span1}>page</span>.
           </Typography>
-          <Button color="inherit">Login</Button>
+          <br />
+          <Typography
+            variant="p"
+            sx={{
+              display: { xs: "none", md: "flex" },
+              fontFamily: "lato",
+              fontWeight: 400,
+              color: "black",
+            }}
+          >
+            Making Memories Live
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            ></Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Avatar alt="" src={avatar} />
+          </Box>
         </Toolbar>
-      </AppBar>
-    </Box>
+      </Container>
+    </AppBar>
   );
 }
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,10 @@
-import ButtonAppBar from "../Components/Navbar";
+import Appbar from "../Components/Navbar";
 import React from "react";
 
 export default function Homepage() {
   return (
     <div>
-      <ButtonAppBar />
+      <Appbar />
     </div>
   );
 }
