a519e0bd89abd994ba8b0c50f18b1f8c51375e91
vimansha <vimanshamadushani88@gmail.com>
2024-03-15 22:56:58 +0530

    feat: data picker component to the feed.js

---
+++ src/Components/DatePicker.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+export default function TheDatePicker({ label, className }) {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker label={label} className={className} />
+    </LocalizationProvider>
+  );
+}
--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -2,6 +2,8 @@ import React from "react";
 import Box from "@mui/material/Box";
 import FilledButton from "./FilledButton";
 import SearchBtn from "./SearchBtn";
+import TheDatePicker from "./DatePicker";
+import classes from "./Feed.module.css";
 
 export default function Feed() {
   return (
@@ -12,7 +14,9 @@ export default function Feed() {
         margin: "50px 20px",
       }}
     >
-      {/* <FilledButton buttonText={"Place a Notice"} /> */}
+      {/* <TheDatePicker label={"From"} className={classes.datePicker} />
+      <TheDatePicker label={"To"} className={classes.datePicker} />
+      <FilledButton buttonText={"Place a Notice"} /> */}
     </Box>
   );
 }
